cbd9d0f06f7190abf0db29f0951db37caa0dcf44
jefferson kwak <jeffersonkr@hotmail.com>
2020-06-03 15:25:12 -0300

    add people class

---
+++ models/people.py
@@ -0,0 +1,18 @@
+class People:
+    def __init__(self, name, age):
+        self._name = name
+        self._age = age
+
+    def __str__(self):
+        return self._name
+
+    @property
+    def age(self):
+        return self._age
+
+    def eat(self):
+        print(f'{self._name} is eating')
+
+    def get_older(self, year=1):
+        self._age += year
+        
